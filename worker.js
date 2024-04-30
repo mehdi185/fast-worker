054b9966735ecb109ee6a9aca7e20cabc55ffd9e
mehdi185 <76259014+mehdi185@users.noreply.github.com>
2024-04-30 04:36:30 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -9,7 +9,7 @@ let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 const bestCFIP = "172.67.99.203"
 
 // 用于 CF 网站的代理 IP
-const proxyIPs = ["cdn.xn--b6gac.eu.org"]; // const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
+const proxyIPs = ['cdn.xn--b6gac.eu.org']; // const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
